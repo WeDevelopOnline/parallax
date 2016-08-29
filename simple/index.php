9ac4dee416b019ff72a22f17743f944c9b5b8e97
Vikash Mishra <vikashjhakash@users.noreply.github.com>
2016-08-29 23:44:03 +0530

    Uploading Simple Parallax website template
    This site can be used for simple and good looking one page brochure website.

---
+++ simple/index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Hawk Eyes</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<!-- Start WOWSlider.com HEAD section -->
+	<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+	<!-- End WOWSlider.com HEAD section -->
+	
+	<style>
+	@import 'https://fonts.googleapis.com/css?family=Droid+Sans|Josefin+Sans|Noto+Sans';
+	
+	html, body, p {
+		font-family: 'Droid Sans', sans-serif;
+		color:#808080;
+	}
+	
+	</style>
+	
+	<script>
+		$(function() {
+		  $('a[href*="#"]:not([href="#"])').click(function() {
+			if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+			  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html, body').animate({
+				  scrollTop: target.offset().top
+				}, 1000);
+				return false;
+			  }
+			}
+		  });
+		});
+	</script>
+
+</head>
+
+<body>
+
+	<nav class="navbar navbar-default navbar-fixed-top" >
+	  <div class="container-fluid">
+		<div class="navbar-header">
+		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span>
+			<span class="icon-bar"></span> 
+		  </button>
+		  <a class="navbar-brand" href="#">Hawk Eyes</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+		  <ul class="nav navbar-nav  navbar-right">
+			<li><a href="#home"><span class="glyphicon glyphicon-home"></span> Home</a></li>
+			<li><a href="#wwa">WHO WE ARE?</a></li> 
+			<li><a href="#wwo">WHAT WE OFFER?</a></li>
+			<li><a href="#op">OUR PRODUCTS?</a></li> 
+			<li><a href="#cu">CONTACT US</a></li> 
+		  </ul>
+		</div>
+	  </div>
+	</nav>
+	
+
+<div id="home">
+	<!-- Start WOWSlider.com BODY section -->
+		<div id="wowslider-container1" style="padding-top:50px;">
+		<div class="ws_images"><ul>
+				<li><img src="data1/images/home.jpg" alt="Home Security" title="Home Security" id="wows1_0"/>We provide the best in market Home Security</li>
+				<li><img src="data1/images/homecctv.jpg" alt="Best In Class Security Devices" title="Best In Class Security Devices" id="wows1_1"/>The Devices that Suits your Standards and your Life Style</li>
+				<li><img src="data1/images/seccam.jpg" alt="End 2 End Security" title="End 2 End Security" id="wows1_2"/>We provide End To End Security Devices.</li>
+				<li><img src="data1/images/tab.jpg" alt="All at your Finger Tips" title="All at your Finger Tips" id="wows1_3"/>It's as Simple as you think.</li>
+				<li><img src="data1/images/chanel.jpg" alt="School RFID System" title="School RFID System" id="wows1_4"/>We care for your child too. 
+		Get the latest RFID solutions to have updates on your child entering and leaving the school premises.</li>
+				<li><a href="http://wowslider.com/vi"><img src="data1/images/mob.jpg" alt="jquery carousel" title="SMS Alerts" id="wows1_5"/></a>Get SMS Alarts when your child enters or leaves the school premises.</li>
+				<li><img src="data1/images/security.png" alt="SECURITY That Does MATTERS ALOT." title="SECURITY That Does MATTERS ALOT." id="wows1_6"/></li>
+			</ul></div>
+			<div class="ws_bullets"><div>
+				<a href="#" title="Home Security"><span><img src="data1/tooltips/home.jpg" alt="Home Security"/>1</span></a>
+				<a href="#" title="Best In Class Security Devices"><span><img src="data1/tooltips/homecctv.jpg" alt="Best In Class Security Devices"/>2</span></a>
+				<a href="#" title="End 2 End Security"><span><img src="data1/tooltips/seccam.jpg" alt="End 2 End Security"/>3</span></a>
+				<a href="#" title="All at your Finger Tips"><span><img src="data1/tooltips/tab.jpg" alt="All at your Finger Tips"/>4</span></a>
+				<a href="#" title="School RFID System"><span><img src="data1/tooltips/chanel.jpg" alt="School RFID System"/>5</span></a>
+				<a href="#" title="SMS Alerts"><span><img src="data1/tooltips/mob.jpg" alt="SMS Alerts"/>6</span></a>
+				<a href="#" title="SECURITY That Does MATTERS ALOT."><span><img src="data1/tooltips/security.png" alt="SECURITY That Does MATTERS ALOT."/>7</span></a>
+			</div></div><div class="ws_shadow"></div>
+		</div>	
+		<script type="text/javascript" src="engine1/wowslider.js"></script>
+		<script type="text/javascript" src="engine1/script.js"></script>
+	<!-- End WOWSlider.com BODY section -->
+</div>	
+
+	<div class="container-fluid" style="height:400px; background:#fff;" id="wwa">
+		
+			<div class="container" style="text-align:justify; padding-top:50px;">
+				<h1 style="text-align:center; font-family: 'Josefin Sans', sans-serif; color:#66ccff">WHO WE ARE??</h1><br/>
+				<p><strong>Hawk Eyes Security Solutions</strong> does an amazing job of providing its customer complete range of security services in <strong>Schools, offices, shopping complexes, hospitals, homes and many more</strong>. Company provides every service related to security ranging from <strong>RFID I-cards, CCTV’s, Home security, etc.</strong></p>
+				<p>We believe in full customer satisfaction and pledged to give best services at <strong>most reasonable prices</strong>. We operate country wide and have our offices in all the major cities with </strong>full customer care support</strong>.</p>
+					
+					
+			</div>
+				
+			</div>
+		
+	</div>
+	
+	
+	<div class="container-fluid" style="height:400px; background:URL('wwa.png')no-repeat fixed; background-size:cover;" id="wwo">
+		<div class="container">
+			<div class="row " style="padding-top:100px;">
+				<div class="col-sm-8">
+					<div style="padding:30px; background:#66ccff; opacity:.8; border-radius:0 35px 0 35px; border: 2px solid #eee; box-shadow: 5px 5px 5px #808080; //height: 200px;">
+						<h1 style="color:#6666ff"> What We Offer</h1>
+						<ul>
+							<li><strong>RFID Attendance System </li>
+							<li>Biometric Attendance System </li>
+							<li>Complete Home Security Package </li>
+							<li>CCTV’s</strong></li> 
+						</ul>
+					</div>
+				</div>
+				
+				<div class="col-sm-4">
+					
+				</div>
+				
+			</div>
+		</div>
+	</div>
+
+	
+	<div class="container-fluid" style="//height:400px; background:#000099; padding-bottom:30px;" id="op">
+		<div class="container">
+			<div class="row " style="padding-top:80px; padding-bottom:20px;">
+				<div class="col-sm-12">
+					<h1 style="color:#fff; font-family: 'Josefin Sans', sans-serif; text-align:center"><strong>Our Products</strong></h1>
+				</div>
+			</div>
+			
+			<div class="row " >
+				<div class="col-sm-4" style="color:#fff; padding:20px;">
+					<img src="homeSecurity.png" class="img-responsive" style="max-height:100px; margin:auto auto;"/><br/>
+					<h3 style="color:#fff; font-family: 'Josefin Sans', sans-serif; text-align:center">Home Security</h3>
+					<p style="color:#fff">Some discription</p>
+					<ul>
+						<li>device 1</li>
+						<li>device 2</li>
+					</ul>
+					<a href="" class="btn btn-info btn-sm">Know More</a>
+				</div>
+				
+				<div class="col-sm-4" style="color:#fff; padding:20px;">
+					<img src="rfid.png" class="img-responsive" style="max-height:100px; margin:auto auto;"/><br/>
+					<h3 style="color:#fff; font-family: 'Josefin Sans', sans-serif; text-align:center">RFID School Attendance System</h3>
+					<p style="color:#fff">Some discription</p>
+					<ul>
+						<li>device 1</li>
+						<li>device 2</li>
+					</ul>
+					<a href="" class="btn btn-info btn-sm">Know More</a>
+				</div>
+				
+				<div class="col-sm-4" style="color:#fff; padding:20px;">
+					<img src="camera.png" class="img-responsive" style="max-height:100px; margin:auto auto;"/><br/>
+					<h3 style="color:#fff; font-family: 'Josefin Sans', sans-serif; text-align:center">Office Security</h3>
+					<p style="color:#fff">Some discription</p>
+					<ul>
+						<li>device 1</li>
+						<li>device 2</li>
+					</ul>
+					<a href="" class="btn btn-info btn-sm">Know More</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-fluid">
+		<div class="container">
+			<div class="row " style="padding-top:20px; //padding-bottom:20px;" id="cu">
+				<div class="col-sm-12">
+					<h1 style="font-family: 'Josefin Sans', sans-serif; text-align:center"><strong>Contact Us</strong></h1>
+				</div>
+			</div>
+			
+			<div class="row " style="padding-top:10px;">
+				<div class="col-sm-4">
+					<h3 style="font-family: 'Josefin Sans', sans-serif; text-align:center"><strong>Feedback</strong></h3>
+					<div class="form-group">
+						<form method="POST">
+							<label for="name">Name:</label>
+							<input type="text" class="form-control" id="name" placeholder="Your Full Name">
+							<label for="email">Email Address:</label>
+							<input type="email" class="form-control" id="name" placeholder="Your Email Address">
+							<label for="comment">Comment:</label>
+							<textarea class="form-control" rows="5" id="comment" placeholder="We are happy to hear from you... Do write us your Feedbacks."></textarea>
+							<input type="submit" class="btn btn-info" id="submit">
+						</form>
+					</div>
+				</div>
+				
+				<div class="col-sm-4">
+					<h3 style="font-family: 'Josefin Sans', sans-serif; text-align:center"><strong>Write Us</strong></h3>
+					<div class="form-group">
+						<form method="POST">
+							<label for="name">Name:</label>
+							<input type="text" class="form-control" id="name" placeholder="Your Full Name">
+							<label for="email">Email Address:</label>
+							<input type="email" class="form-control" id="name" placeholder="Your Email Address">
+							<label for="comment">Message:</label>
+							<textarea class="form-control" rows="5" id="comment" placeholder="We will be happy to help you, write your query here."></textarea>
+							<input type="submit" class="btn btn-info" id="submit">
+						</form>
+					</div>
+				</div>
+				
+				<div class="col-sm-4" style="text-align:center">
+					<h3 style="font-family: 'Josefin Sans', sans-serif; text-align:center"><strong>Call Us / Mail Us / Visit Us</strong></h3>
+					<br/>
+					<p>Also you can write to us at :
+					<span style="color:#ff6d06"><strong>support@hawkeyes.com</strong></span></p>
+					<p>OR Call us at :
+					<span style="color:#ff6d06"><strong>+91 xxxx xxxx xx</strong></span></p>
+					<p>OR Visit us at :
+					<span style="color:#ff6d06"><strong>xyz city, mno street<br/>abc place (700055)</strong></span></p>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container-fluid" style="height:30px; background:#eee;">
+		<div class="container" style="text-align:center; padding:10px">
+			<span style="" >
+			&copy; HAWK EYES 2016 || ALL RIGHTS RESERVED
+			</span>
+		</div>
+	</div>
+	
+	
+	
+</body>
+
+</html>
